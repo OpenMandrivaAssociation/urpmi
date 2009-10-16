OpenMandrivaAssociation/urpmi

commit 30ec54de7d294c90cd718c53b3abdbcdb1f7e0ed

6.29: - downgrade skipped package log message to debug message - make sure we don't check certificate in aria2 except when we want to - exclude kernel-source from orphan processing (#53426) - do not list as orphans kernel packages which where not installed through   dependencies (#53425)
SILENT: svn-revision: 454171

urpmi.spec
@@ -12,7 +12,7 @@
 %{?!makeinstall_std: %define makeinstall_std() make DESTDIR=%{?buildroot:%{buildroot}} install}
 
 %define name	urpmi
-%define version	6.28.2
+%define version	6.29
 %define release	%mkrel 1
 
 %define group %(perl -e 'print "%_vendor" =~ /\\bmandr/i ? "System/Configuration/Packaging" : "System Environment/Base"')

commit cd43821e86b1307c0743af1f2c30c070e623d1ab

6.30: - unbreak kernel orphans management (broken by #53425 fix) - improve messages asking to restart system/session (#53126) - add --download-all option to download all packages before attempting   to start installation - fix priviledge escalation in rurpmi and rurpme (#54568) - when the database is locked, print the PID of the processus locking it  (#38923, Pascal Terjan)
SILENT: svn-revision: 457917

urpmi.spec
@@ -12,7 +12,7 @@
 %{?!makeinstall_std: %define makeinstall_std() make DESTDIR=%{?buildroot:%{buildroot}} install}
 
 %define name	urpmi
-%define version	6.29
+%define version	6.30
 %define release	%mkrel 1
 
 %define group %(perl -e 'print "%_vendor" =~ /\\bmandr/i ? "System/Configuration/Packaging" : "System Environment/Base"')